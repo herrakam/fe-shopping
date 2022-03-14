a87f7cd1fa18e62cca2d0a9b50abdb0a1ee45b8e
SeungHyun <fm10033@gmail.com>
2022-03-14 12:09:52 +0900

    feat : 기본 구조 형성
    express generator로 기본 구조 형성
    scss, cors 설치
    view 파일  html로 바꿈

---
+++ coupang/app.js
@@ -0,0 +1,17 @@
+const express = require("express");
+// const router = require("./routes/dataRouter.js");
+const app = express();
+const cors = require("cors");
+const PORT = 3000;
+app.use(cors());
+app.use(express.static(__dirname + "/public"));
+// app.use("/", router);
+
+app.get("/", (req, res) => {
+  res.sendFile(__dirname + "/" + "views/main.html");
+});
+
+app.listen(PORT, (err) => {
+  if (err) return console.log(err);
+  console.log("The server is listening on port 3000");
+});
